8d1849c60123bdf4f8fa21a9ce3e1e016a94d4dc
Miguel Añez <migueaanez@gmail.com>
2024-10-19 12:51:31 -0400

    log and sign fixed

--- registro/views.py
+++ registro/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from django.contrib.auth.models import UserCreationForm
-from django.views.generic import CreateView, TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
 # Create your views here.
 
-class signupView(CreateView):
+class SignUpView(CreateView):
   template_name = "registration/signup.html"
   form_class = UserCreationForm
   success_url = reverse_lazy("login")
 
-class LogoutView(TemplateView):
-  template_name = "logout.html"
+def FirstPage(request):
+  return render(request, "first_page.html")
